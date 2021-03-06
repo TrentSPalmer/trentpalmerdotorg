f9f8bf13ac2f7e88f88e7beaf8aac2f04b069e7e
Trent Palmer <trenttdually@gmail.com>
2021-03-05 20:12:02 -0800

    add email backend to settings.py

--- tp/settings.py
+++ tp/settings.py
@@ -149,3 +149,10 @@ IMAGES_FILE_STORAGE = 'tp.storage_backends.PublicImageStorage'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 LOGGING = init_logging_settings()
+
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == "True"
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
