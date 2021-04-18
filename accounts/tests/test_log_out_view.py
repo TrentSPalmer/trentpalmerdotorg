58a1e87db8971d12a9ec69ebebba8a6a34d7cc24
Trent Palmer <trenttdually@gmail.com>
2021-04-18 16:27:45 -0700

    update accounts/tests/test_log_out_view.py

--- accounts/tests/test_log_out_view.py
+++ accounts/tests/test_log_out_view.py
@@ -13,14 +13,14 @@ class TestLogOutViewTestCase(TestCase):
     def test_log_out_view(self):
         self.client.login(username='user_a', password='password_user_a')
         response = self.client.post(reverse('accounts:logout'), follow=True)
-        self.assertTrue("Successfully Logged Out" in response.content.decode('utf-8'))
         self.assertEquals(response.status_code, 200)
+        self.assertTrue("Successfully Logged Out" in response.content.decode('utf-8'))
         self.assertTemplateUsed(response, 'audio/index.html')
         self.assertEquals(response.request['PATH_INFO'], '/')
 
     def test_log_out_view_no_login(self):
         response = self.client.post(reverse('accounts:logout'), follow=True)
-        self.assertFalse("Successfully Logged Out" in response.content.decode('utf-8'))
         self.assertEquals(response.status_code, 200)
+        self.assertFalse("Successfully Logged Out" in response.content.decode('utf-8'))
         self.assertTemplateUsed(response, 'audio/index.html')
         self.assertEquals(response.request['PATH_INFO'], '/')
