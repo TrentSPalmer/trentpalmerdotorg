95cb13a697a2c6dc5f5fbe47dd70035d309f7b05
Trent Palmer <trenttdually@gmail.com>
2021-04-22 20:02:07 -0700

    update audio/tests/test_confirm_delete_episode_view.py add function test_confirm_delete_episode_view_get()

--- audio/tests/test_confirm_delete_episode_view.py
+++ audio/tests/test_confirm_delete_episode_view.py
@@ -22,6 +22,22 @@ class TestConfirmDeleteEpisodeViewTestCase(TestCase):
         self.assertEquals(len(qs), 1)
         self.assertEquals(response.request['PATH_INFO'], '/')
 
+    def test_confirm_delete_episode_view_get(self):
+        episode_a = Episode.objects.get(title="Mark Twain The Bee")
+        self.client.login(username='user_b', password='password_user_b')
+        kw_args = {'pk': episode_a.pk}
+        response = self.client.get(
+            reverse('audio:confirm_delete_episode', kwargs=kw_args),
+            follow=True)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'audio/delete_episode_confirmation.html')
+        qs = Episode.objects.filter(title="Mark Twain The Bee")
+        self.assertEquals(len(qs), 1)
+        self.assertEquals(
+            response.request['PATH_INFO'],
+            f'/confirm-delete-episode/{episode_a.pk}'
+        )
+
     def test_confirm_delete_episode_view(self):
         episode_a = Episode.objects.get(title="Mark Twain The Bee")
         self.client.login(username='user_b', password='password_user_b')
