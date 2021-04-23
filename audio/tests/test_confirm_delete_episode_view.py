047e2c43928f64a2df43ff37a1dd11aa02ab9ebf
Trent Palmer <trenttdually@gmail.com>
2021-04-22 19:53:20 -0700

    update audio/tests/test_confirm_delete_episode_view.py add function test_confirm_delete_episode_view_wrong_user()

--- audio/tests/test_confirm_delete_episode_view.py
+++ audio/tests/test_confirm_delete_episode_view.py
@@ -9,6 +9,19 @@ class TestConfirmDeleteEpisodeViewTestCase(TestCase):
     def setUp(self):
         set_up()
 
+    def test_confirm_delete_episode_view_wrong_user(self):
+        episode_a = Episode.objects.get(title="Mark Twain The Bee")
+        self.client.login(username='user_a', password='password_user_a')
+        kw_args = {'pk': episode_a.pk}
+        response = self.client.post(
+            reverse('audio:confirm_delete_episode', kwargs=kw_args),
+            follow=True)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'audio/index.html')
+        qs = Episode.objects.filter(title="Mark Twain The Bee")
+        self.assertEquals(len(qs), 1)
+        self.assertEquals(response.request['PATH_INFO'], '/')
+
     def test_confirm_delete_episode_view(self):
         episode_a = Episode.objects.get(title="Mark Twain The Bee")
         self.client.login(username='user_b', password='password_user_b')
