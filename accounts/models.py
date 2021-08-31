ffca5b3c27d50a7a60f20585d86310c160df9205
Trent Palmer <trenttdually@gmail.com>
2021-08-30 21:54:40 -0700

    pyotp.random_base32() has increased their default output length

--- accounts/models.py
+++ accounts/models.py
@@ -18,7 +18,7 @@ class EmailWhiteList(UUIDAsIDModel):
 
 class Account(UUIDAsIDModel):
     user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
-    totp_key = models.CharField(max_length=16, null=True)
+    totp_key = models.CharField(max_length=32, null=True)
     use_totp = models.BooleanField(default=False)
     twitter_handle = models.CharField(max_length=64, default='@Twitter')
 
