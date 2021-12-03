a26348b00109ef97f56945cd1a654758c3a6461c
Trent Palmer <trenttdually@gmail.com>
2021-12-02 22:48:11 -0800

    formatting audio/urls.py

--- audio/urls.py
+++ audio/urls.py
@@ -11,12 +11,22 @@ urlpatterns = [
     path('new-feed/', new_feed, name='new_feed'),
     path('feeds/', views.feeds, name='feeds'),
     path('edit-feed/<str:pk>/<str:title_slug>', edit_feed, name='edit_feed'),
-
-    path('confirm-delete-feed/<str:pk>', confirm_delete_feed, name='confirm_delete_feed'),
-    path('confirm-delete-episode/<str:pk>', confirm_delete_episode, name='confirm_delete_episode'),
-
-    path('new-episode/<str:feed_pk>/<str:feed_title_slug>', new_episode, name='new_episode'),
-    path('edit-episode/<str:pk>/<str:title_slug>', edit_episode, name='edit_episode'),
+    path(
+        'confirm-delete-feed/<str:pk>', confirm_delete_feed,
+        name='confirm_delete_feed'
+    ),
+    path(
+        'confirm-delete-episode/<str:pk>', confirm_delete_episode,
+        name='confirm_delete_episode'
+    ),
+    path(
+        'new-episode/<str:feed_pk>/<str:feed_title_slug>', new_episode,
+        name='new_episode'
+    ),
+    path(
+        'edit-episode/<str:pk>/<str:title_slug>', edit_episode,
+        name='edit_episode'
+    ),
     path('rss/<str:slug>.xml', AudioRssFeed(), name='rss'),
     path('feed/<str:pk>/<str:slug>', views.feed, name='feed'),
     path('episode/<str:pk>/<str:slug>', views.episode, name='episode'),
